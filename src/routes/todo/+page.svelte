138b119a24595b267c5103f973934ba95f31bc36
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-03-04 17:47:13 +0000

    add css

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -10,6 +10,7 @@
 		<label>
 			add a todo:
 			<input
+			class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 				name="description"
 				autocomplete="off"
 			/>
@@ -32,7 +33,8 @@
 		margin: 50px auto;
 		background: beige;
         padding: 20px;
-		box-shadow: 0 0 20px rgba(0,0,0,1);;
+		box-shadow: 0 0 20px rgba(0,0,0,1);
+		min-width: 33%;
 	}
 
 	label {
