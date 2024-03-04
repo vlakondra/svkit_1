26bc638ac07e074c5317dbc1976f0a84a2e2ce4b
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-03-04 17:00:01 +0000

    Cross-site POST forbidden

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <nav>
 	<a class='link' href="/">home</a>
+	<a class='link' href="/todo">ToDo</a>
 	<a class='link' href="/blog">blog</a>
 	<a class='link' href="/python">Python</a>
 	<a class='link' href="/countries">Countries</a>
