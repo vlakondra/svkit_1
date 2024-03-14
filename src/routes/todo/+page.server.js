1706b00f539cd5738876017199f1118b2f9368ec
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-03-14 15:56:49 +0000

    forms_actions

--- src/routes/todo/+page.server.js
+++ src/routes/todo/+page.server.js
@@ -14,9 +14,13 @@ export function load({ cookies }) {
 }
 
 export const actions = {
-	default: async ({ cookies, request }) => {
+	create: async ({ cookies, request }) => {
 		const data = await request.formData();
-		console.log(data)
 		db.createTodo(cookies.get('userid'), data.get('description'));
+	},
+
+	delete: async ({ cookies, request }) => {
+		const data = await request.formData();
+		db.deleteTodo(cookies.get('userid'), data.get('id'));
 	}
 };
--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -5,26 +5,33 @@
 <div class="centered">
 	<h1>todos</h1>
 
-	<form method="POST" class="max-w-sm mx-auto">
-		<div class="relative z-0 w-full mb-5 group">
-		<label>
-			add a todo:
-			<input
-			class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				name="description"
-				autocomplete="off"
-			/>
-		</label>
+	<form method="POST" action="?/create" class="mx-auto max-w-sm">
+		<div class="group relative z-0 mb-5 w-full">
+			<label>
+				add a todo:
+				<input
+					class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+					name="description"
+					autocomplete="off"
+				/>
+			</label>
 		</div>
 	</form>
 
-	<ul class="todos">
+	<div class="todos">
 		{#each data.todos as todo (todo.id)}
-			<li>
-				{todo.description}
-			</li>
+			<!-- <li> -->
+			<form method="POST" action="?/delete">
+				<input type="hidden" name="id" value={todo.id} />
+				<div style="display:flex;width:100%">
+					<div style="flex:1">{todo.description}</div>
+
+					<button aria-label="Mark as complete" />
+				</div>
+			</form>
+			<!-- </li> -->
 		{/each}
-	</ul>
+	</div>
 </div>
 
 <style>
@@ -32,8 +39,8 @@
 		max-width: 20em;
 		margin: 50px auto;
 		background: beige;
-        padding: 20px;
-		box-shadow: 0 0 20px rgba(0,0,0,1);
+		padding: 20px;
+		box-shadow: 0 0 20px rgba(0, 0, 0, 1);
 		min-width: 33%;
 	}
 
@@ -51,13 +58,14 @@
 
 	button {
 		border: none;
-		background: url(./remove.svg) no-repeat 50% 50%;
+		background: url('remove.svg') no-repeat 50% 50%;
 		background-size: 1rem 1rem;
 		cursor: pointer;
 		height: 100%;
+		width: 1rem;
 		aspect-ratio: 1;
 		opacity: 0.5;
-		transition: opacity 0.2s;
+		transition: opacity 1.2s;
 	}
 
 	button:hover {
