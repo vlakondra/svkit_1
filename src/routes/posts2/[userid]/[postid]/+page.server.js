f43c8ede5a498c0bad4d2c39e0749e898280a199
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-05-26 11:39:37 +0000

    add comments

--- src/routes/posts2/[userid]/[postid]/+page.server.js
+++ src/routes/posts2/[userid]/[postid]/+page.server.js
@@ -3,10 +3,18 @@ async function getPost(id) {
      return response.json()
   }
   
+  async function getComments(id) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+     return response.json()
+  }
+
   export async function load({params}) {
       let post = await getPost(params.postid)
+      let comments =  await getComments(params.postid)
+
       return {
         params:params.postid,
-        post:post
+        post:post,
+        comments:comments
       }
     }
--- src/routes/posts2/[userid]/[postid]/+page.svelte
+++ src/routes/posts2/[userid]/[postid]/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-    export let data;
-    import { page } from '$app/stores';
+	export let data;
+	import { page } from '$app/stores';
+	let showComm = false;
+	const TriggerComment = () => {
+		showComm = !showComm;
+	};
 </script>
 
-
+<!-- {JSON.stringify(data.comments)} -->
 <p>{data.post.body}</p>
+<div>
+	<button on:click={TriggerComment}>
+		{#if !showComm}
+			Показать
+		{:else}
+			Скрыть
+		{/if}
+	</button>
+</div>
+<div>
+	{#if showComm}
+		{#each data.comments as comm}
+			<div>
+				{comm.body}
+			</div>
+		{/each}
+	{/if}
+</div>
